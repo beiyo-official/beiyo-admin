7191d4ea4a4dc4a19c8093e469057b9516a9ab70
DakshBaxi <baxidaksh2004@gmail.com>
2024-05-18 11:04:43 +0530

    updation of room bed done

--- server/routes/Bed.js
+++ server/routes/Bed.js
@@ -3,51 +3,11 @@ const router = express.Router();
 const Beds = require('../models/Beds');
 const Room = require('../models/Room');
 
-// Add a new bed to a room
-router.post('/rooms/:roomId/beds', async (req, res) => {
-  const { roomId } = req.params;
-//   const { isEmpty, charge, availableFrom, paymentStatus, duration, dueDate } = req.body;
 
-  try {
-    const room = await Room.findById(roomId);
-    if (!room) {
-      return res.status(404).json({ error: 'Room not found' });
-    }
-
-    // if (room.beds.length >= room.capacity) {
-    //   return res.status(400).json({ error: 'Cannot add more beds than room capacity' });
-    // }
-
-    const beds = [];
-    for (let i = 1; i <= room.capacity; i++) {
-      const bed = new Beds({ roomId: room._id, bedNumber: `Bed ${i}`,roomNumber: room.roomNumber });
-      await bed.save();
-      beds.push(bed._id);
-    }
-
-    // Update room with created beds
-    room.beds = beds;
-    await room.save();
-
-
-    // const bed = new Bed({ roomId, isEmpty, charge, availableFrom, paymentStatus, duration, dueDate });
-    // await bed.save();
-
-    // room.beds.push(bed._id);
-    // room.remainingCapacity -= 1;
-    // await room.save();
-
-    res.status(201).json(beds);
-  } catch (error) {
-    console.error('Error adding bed:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
 
 // Get all beds for a room
 router.get('/rooms/:roomId/beds', async (req, res) => {
   const { roomId } = req.params;
-
   try {
     const beds = await Beds.find({ roomId });
     res.json(beds);
@@ -56,9 +16,18 @@ router.get('/rooms/:roomId/beds', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-
+router.get('/:bedId',async(req,res)=>{
+  const {bedId}=req.params;
+  try{
+    const beds = await Beds.find({ bedId });
+    res.json(beds);
+  } catch (error) {
+    console.error('Error fetching beds:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+})
 // Update bed details
-router.patch('/beds/:bedId', async (req, res) => {
+router.patch('/:bedId', async (req, res) => {
   const { bedId } = req.params;
   const updates = req.body;
 
@@ -75,7 +44,7 @@ router.patch('/beds/:bedId', async (req, res) => {
 });
 
 // Delete a bed
-router.delete('/beds/:bedId', async (req, res) => {
+router.delete('/:bedId', async (req, res) => {
   const { bedId } = req.params;
 
   try {
