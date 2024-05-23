8b0f29e948516cdf844a5cb52aebc71db8f7a72c
DakshBaxi <baxidaksh2004@gmail.com>
2024-05-23 12:56:26 +0530

    sorting data

--- server/routes/roomRoutes.js
+++ server/routes/roomRoutes.js
@@ -7,7 +7,7 @@ const Beds = require('../models/Beds');
 // Get all rooms
 router.get('/', async (req, res) => {
   try {
-    const rooms = await Room.find();
+    const rooms = await Room.find().sort({name:1});
     res.json(rooms);
   } catch (err) {
     res.status(500).json({ message: err.message });
