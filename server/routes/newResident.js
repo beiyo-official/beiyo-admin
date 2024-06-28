09ffb6cf17f61ff487104a4895d713b4af22f88b
DakshBaxi <baxidaksh2004@gmail.com>
2024-06-28 14:16:00 +0530

    save resident

--- server/routes/newResident.js
+++ server/routes/newResident.js
@@ -10,7 +10,7 @@ const router = express.Router();
 
  router.post('/',async(req,res)=>{
     try {
-        const {studentData} = req.body
+        const {studentData} = req.query
     const newResident = new Resident(JSON.parse(studentData));
     await newResident.save();
     } catch (error) {
