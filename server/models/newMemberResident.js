27d4643eff29eb6c29f19fb64b8d850f7e25c95c
DakshBaxi <baxidaksh2004@gmail.com>
2024-07-16 17:54:44 +0530

    form done

--- server/models/newMemberResident.js
+++ server/models/newMemberResident.js
@@ -5,9 +5,15 @@ const residentSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   mobileNumber: { type: Number, required: true },
-  address: { type: String, required: true },
-  parentsName: { type: String, required: true },
-  parentsMobileNo: { type: Number, required: true },
+  address: { type: String,
+    //  required: true 
+    },
+  parentsName: { type: String, 
+    // required: true 
+  },
+  parentsMobileNo: { type: Number, 
+    // required: true 
+  },
   hostel: { type: String, required: true },
   roomNumber: { type: String, required: true },
   dateJoined:{type:Date},
