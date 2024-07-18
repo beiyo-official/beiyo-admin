f119698a208f3f400dd446dec8a5980689df5b05
DakshBaxi <baxidaksh2004@gmail.com>
2024-07-18 16:14:41 +0530

    payment id done

--- server/routes/DashBoard.js
+++ server/routes/DashBoard.js
@@ -21,12 +21,17 @@ router.get('/payments/:userId', async (req, res) => {
   }
 });
 
-router.get('/payments/:id', async (req, res) => {
+router.get('/payment/:id', async (req, res) => {
   try {
-    const payments = await Payment.findById({id});
-    res.json(payments);
+    const paymentId = req.params.id;
+    console.log(paymentId);
+    const payment = await Payment.findById(paymentId);
+    if (!payment) {
+      return res.status(404).json({ message: 'Payment not found' });
+    }
+    res.json(payment);
   } catch (error) {
-    console.error('Error fetching payments:', error);
+    console.error('Error fetching payment:', error);
     res.status(500).send('Internal Server Error');
   }
 });
