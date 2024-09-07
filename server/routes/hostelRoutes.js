4cabedaad341f38a6da3fa20f3029a22a223a8ba
DakshBaxi <baxidaksh2004@gmail.com>
2024-09-07 13:40:58 +0530

    get all hostels

--- server/routes/hostelRoutes.js
+++ server/routes/hostelRoutes.js
@@ -76,32 +76,47 @@ router.get('/managerTickets/:hostelId',async(req,res)=>{
 
 
 
-
-
+// Get all hostels
 router.get('/', async (req, res) => {
   try {
-    // await totalRooms();
-    // await totalBeds();
-
-    const { page = 1, limit = 10 } = req.query; // Default to page 1 and limit of 10
-
-    const hostels = await Hostel.find()
-      .sort({ totalRemainingBeds: -1, name: 1 })
-      .skip((page - 1) * limit)
-      .limit(parseInt(limit));
-
-    const totalHostels = await Hostel.countDocuments(); // Total number of hostels
-
-    res.json({
-      hostels,
-      currentPage: page,
-      totalPages: Math.ceil(totalHostels / limit),
-      totalHostels,
-    });
+  await  totalRooms();
+  await  totalBeds();
+  // await mappingResidentToHostel();
+  const { page = 1, limit = 10  } = req.params;
+  const hostels = await Hostel.find()
+  .sort({ totalRemainingBeds: -1, name: 1 })
+  .skip((page - 1) * limit)
+  .limit(parseInt(limit));
+    res.json(hostels);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 });
+
+// router.get('/', async (req, res) => {
+//   try {
+//     // await totalRooms();
+//     // await totalBeds();
+
+//     const { page = 1, limit = 10 } = req.query; // Default to page 1 and limit of 10
+
+//     const hostels = await Hostel.find()
+//       .sort({ totalRemainingBeds: -1, name: 1 })
+//       .skip((page - 1) * limit)
+//       .limit(parseInt(limit));
+
+//     const totalHostels = await Hostel.countDocuments(); // Total number of hostels
+
+//     res.json({
+//       hostels,
+//       currentPage: page,
+//       totalPages: Math.ceil(totalHostels / limit),
+//       totalHostels,
+//     });
+//   } catch (err) {
+//     res.status(500).json({ message: err.message });
+//   }
+// });
 
 // update hostels
 router.put('/:id', async (req, res) => {
