44f212e87881f52b394e8125ce95388590b8d896
DakshBaxi <111555273+DakshBaxi@users.noreply.github.com>
2024-09-18 17:26:16 +0530

    Update models member file

---
+++ server/models/Member.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const memberSchema = new Schema({
+  name: { type: String, required: true },
+  email:{ type: String, required: true},
+  post: { type: String, required: true },
+  uniqueId: {type: String, required: true},
+  mobileNumber: { type: Number,  
+  },
+  address: { type: String,
+    }, 
+  imageUrl:{type:String},
+  aadhaarCardUrl: {type:String},
+});
+
+const Staff = mongoose.model('Staff', staffSchema);
+
+module.exports = Member;
+
+
