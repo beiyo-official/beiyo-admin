c4558783ec98ddb0b84cba54a3e6d0d6a16eecb3
DakshBaxi <baxidaksh2004@gmail.com>
2024-10-21 23:34:21 +0530

    booking page and dashboard

--- server/routes/otp.js
+++ server/routes/otp.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
+const newMemberResident = require('../models/newMemberResident');
 
 router.post('/send',async(req,res)=>{
     const {email} = req.body;
+    const resident = await newMemberResident.find(email)
+    if(resident){
+      res.status(400).json("Email already exist");
+    }
     const otp = Math.floor(100000 + Math.random() * 900000);
     await sendUniqueIdEmail(email,otp);
     res.json(otp);
