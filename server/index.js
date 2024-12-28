ca5fae70e7d8a150c1ec9477efac3f41cd7f7bd7
DakshBaxi <baxidaksh2004@gmail.com>
2024-12-28 17:48:43 +0530

    cors policy update 3

--- server/index.js
+++ server/index.js
@@ -31,6 +31,15 @@ const notification = require('./routes/notification')
 const faq = require('./routes/Faq');
 const corsMiddleware = require('./middleware/corsMiddleware');
 const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://beiyo.in');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 app.use(cors(
   corsMiddleware
 ));
@@ -79,13 +88,7 @@ app.get("/", (req,res)=>{
   res.json("Welcome to the Beiyo admin backend");
 });
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://beiyo.in');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+
 
 app.use('/api/hostels', hostelRoutes);
 app.use('/api/rooms', roomRoutes);
